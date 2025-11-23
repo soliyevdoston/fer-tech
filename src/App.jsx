e857aace87f402df2456a462ac30111d4b20f4ca
Dostonbek Solijonov <youremail@example.com>
2025-11-24 00:10:53 +0500

    error2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
-import Search from "./components/Search";
 import Footer from "./components/Footer";
+import Search from "./components/Search";
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -16,10 +16,11 @@ export default function App() {
   }, [darkMode]);
 
   return (
-    <div className="bg-[#DAD7CD] dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-500 min-h-screen">
+    <div className="transition-colors duration-500 min-h-screen">
       <Header darkMode={darkMode} />
+
       <Main darkMode={darkMode} />
-      <Search darkMode={darkMode} id="search" />
+      <Search darkMode={darkMode} />
       <Footer darkMode={darkMode} />
 
       <button
